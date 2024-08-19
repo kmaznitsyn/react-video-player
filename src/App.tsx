f65af040de40fa13e646a0d7666be9b19221d715
kmaznitsyn <mykola.maznitsyn@profitsoft.dev>
2024-08-19 20:45:32 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import ReactPlayer from "react-player";
+
+function validURL(str: string) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
+  return !!pattern.test(str);
+}
 
 function App() {
+  const [videoLink, setVideoLink] = useState(
+    "https://www.youtube.com/watch?v=wIyHSOugGGw"
+  );
+
+  const videoLinkHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setVideoLink(e.target.value);
+  };
+
+  const isValidUrl = validURL(videoLink);
+  console.log(isValidUrl);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col pt-4 items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+      <h1 className="font-bold text-xl py-2 uppercase">React Video</h1>
+      <input
+        className={`p-2 mb-4 rounded-xl ${isValidUrl ? "" : "text-red-500"}`}
+        onChange={videoLinkHandler}
+        value={videoLink}
+        placeholder="Enter valid url"
+      />
+
+      {!videoLink && (
+        <div className="flex items-center justify-center">
+          <p className="font-bold text-lg">
+            Please provide a valid Url for using this React Player
+          </p>
+        </div>
+      )}
+      {videoLink && <ReactPlayer controls url={videoLink} />}
     </div>
   );
 }
